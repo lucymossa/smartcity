0223132fc5a1912166de1402d1d0ed2581915a9e
lucymossa <lucymossa@gmail.com>
2019-10-20 13:47:06 +0100

    slide commit

--- script.js
+++ script.js
@@ -11,19 +11,11 @@ function showSlides(n) {
     if (n > slides.length) {slideIndex = 1}    
     if (n < 1) {slideIndex = slides.length}
     for (var i = 0; i < slides.length; i++) {
-      slides[i].style.opacity  = "0";
+      slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.opacity = "1";
+    slides[slideIndex-1].style.display = "block";
 }
-
-document.getElementById("prev").onclick = function(){
-  showSlides(slideIndex -= 1);
-  
-  window.scrollTo(0, 0);
-};
 
 document.getElementById("next").onclick = function(){
   showSlides(slideIndex += 1);
-  window.scrollTo(0, 0);
-  document.getElementById("prev").style.display  = "block";
 }
