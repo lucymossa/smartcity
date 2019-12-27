210c19d7bd376f900f7c13c3bd4c7eab6671ea82
lucymossa <lucymossa@gmail.com>
2019-12-27 10:43:43 +0000

    smart city update

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-//Push .sub out of frame
-var push = window.innerHeight;
-var start = document.getElementsByClassName("start");
+// //Push .sub out of frame
+// var push = window.innerHeight;
+// var start = document.getElementsByClassName("start");
 
-for (var i = 0; i < start.length; i++) {
-  start[i].style.paddingTop = push + "px";
-}
+// for (var i = 0; i < start.length; i++) {
+//   start[i].style.paddingTop = push + "px";
+// }
 
-window.addEventListener("resize", function(event) {
-  var push = window.innerHeight;
-  for (var i = 0; i < start.length; i++) {
-    start[i].style.paddingTop = push + "px";
-  }
+// window.addEventListener("resize", function(event) {
+//   var push = window.innerHeight;
+//   for (var i = 0; i < start.length; i++) {
+//     start[i].style.paddingTop = push + "px";
+//   }
+// });
+
+// Open and close mobile menu
+var menuIcon = document.getElementById('mobile-menu-opener');
+var menuBody = document.getElementById('mobile-menu');
+var container = document.getElementById('container');
+var bodyElement = document.body;
+menuIcon.addEventListener('click', function(event) {
+	bodyElement.classList.toggle('menu-active');
+});
+container.addEventListener('click', function(event) {
+		bodyElement.classList.remove('menu-active');
+});
+// Mobile touch
+container.addEventListener('touchstart', function(event) {
+		bodyElement.classList.remove('menu-active');
 });
